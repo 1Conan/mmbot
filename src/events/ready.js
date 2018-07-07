0fb7476e22c4612e41e158825792b8851b9c5714
Jellz <xdjellz@gmail.com>
2018-07-07 21:55:53 +1000

    minor syntax edit

--- src/events/ready.js
+++ src/events/ready.js
@@ -8,7 +8,7 @@ module.exports = class extends Event {
         });
     }
 
-    run(...params) {
+    run() {
         this.client.user.setActivity(`for @${this.client.user.username} | ${this.client.guilds.size} servers`, { type: 'WATCHING' });
         this.client.musicQueues = {};
     }
