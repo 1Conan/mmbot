e538fc8d5007e51d5c3a91f76c32db46b9d30e93
Jellz <xdjellz@gmail.com>
2018-07-11 15:27:15 +1000

    this is causing problems

--- src/events/ready.js
+++ src/events/ready.js
@@ -10,22 +10,7 @@ module.exports = class extends Event {
 
     async run() {
         this.client.musicQueues = {};
-        this.client.user.setActivity(`for @${this.client.user.username} | ${this.client.guilds.size} servers`, { type: 'WATCHING' });        
-
-        setInterval(() => { 
-            this.client.user.setActivity(`for @${this.client.user.username} | ${this.client.guilds.size} servers`, { type: 'WATCHING' });
-
-            if (!lists.post) return false;
-
-            if (lists.botsdiscordpw.length > 1) {
-                const botsdiscordpw = await fetch(`https://bots.discord.pw/api/bots/${this.client.id}/stats`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json', 'Authorization': lists.botsdiscordpw },
-                    body: JSON.stringify({ server_count: this.client.guilds.size })
-                });
-                if (!botsdiscordpw.ok) this.client.console.wtf('AHHHH THERE WAS AN ERROR WHILE POSTING STATS TO BOTSDISCORDPW');
-            }
-        }, 60000 * 25);
+        this.client.user.setActivity(`for @${this.client.user.username} | ${this.client.guilds.size} servers`, { type: 'WATCHING' });
     }
 
 };
