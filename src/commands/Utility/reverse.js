91d5ae9b113af9ae3947964022b05984b1069081
Jellz <xdjellz@gmail.com>
2018-07-16 13:04:03 +1000

    clean up args

--- src/commands/Utility/reverse.js
+++ src/commands/Utility/reverse.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
 	constructor(...args) {
 		super(...args, {
             description: 'Reverse text',
-            usage: '<query:str> [...]'
+            usage: '<query:str>'
 		});
 	}
 
